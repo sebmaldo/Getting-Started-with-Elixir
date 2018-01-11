61a4ee9c2a413738b2fbf3d35b7659160b546ddc
Sebastián Maldonado <sebmaldo@gmail.com>
2018-01-11 19:07:58 -0300

    Add local inspect to say_here

--- module_playground.exs
+++ module_playground.exs
@@ -1,9 +1,9 @@
 defmodule ModulePlayground do
     import IO, only: [puts: 1]
     import Kernel, except: [inspect: 1]
-    
+
     def say_here do
-        puts "I'm Here"
+        inspect "I'm Here"
     end
 
     def inspect(param1) do
